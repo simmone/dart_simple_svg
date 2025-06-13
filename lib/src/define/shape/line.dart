7298d8dee644edcc907efa4d7d0213f0e60b791e
Chen Xiao <chenxiao770117@gmail.com>
2025-06-13 10:17:23 +0800

    update

--- lib/src/define/shape/line.dart
+++ lib/src/define/shape/line.dart
@@ -1,6 +1,23 @@
 import 'package:simple_svg/src/define/shape.dart';
 import 'package:simple_svg/src/tool.dart';
 
+/// ![](https://raw.githubusercontent.com/simmone/dart_simple_svg/refs/heads/master/showcase/shapes/line/line.svg)
+///
+/// Example1: define a line by start and end point.
+///```dart
+///    final lineId = svg.defShape(Line((0.0, 0.0), (100.0, 100.0)));
+///
+///    var sstyle = Sstyle();
+///    sstyle.stroke = '#765373';
+///    sstyle.strokeWidth = 10;
+///    var widget = Widget(lineId);
+///    widget.sstyle = sstyle;
+///    widget.at = (5, 5);
+///```
+///
+/// Example2: dual line
+///
+/// ![](https://raw.githubusercontent.com/simmone/dart_simple_svg/refs/heads/master/showcase/shapes/line/dual.svg)
 class Line extends Shape {
   num startX = 0;
   num startY = 0;
