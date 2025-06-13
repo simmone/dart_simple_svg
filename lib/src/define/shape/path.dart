38cb8c315bb10185631c18d4aab9a6fc0cbdc33f
Chen Xiao <chenxiao770117@gmail.com>
2025-06-14 05:28:18 +0800

    update

--- lib/src/define/shape/path.dart
+++ lib/src/define/shape/path.dart
@@ -185,6 +185,104 @@ class PathClose extends PathAction {
   }
 }
 
+/// ![](https://raw.githubusercontent.com/simmone/dart_simple_svg/refs/heads/master/showcase/path/lineto.svg)
+///
+/// Path defined by path actions, abs suffix: absolute position, rel suffix: relative position.
+///
+/// Example1: lineto(linetoHor, linetoVer, linetoRel)
+///
+///```dart
+///    var linePath = Path();
+///    linePath.movetoAbs((5, 5));
+///    linePath.linetoHor(100.0);
+///    linePath.linetoVer(100.0);
+///    linePath.linetoRel((-50.0, 50.0));
+///    linePath.linetoRel((-50, -50));
+///    linePath.close();
+///    final lineId = svg.defShape(linePath);
+///
+///    var lineSstyle = Sstyle();
+///    lineSstyle.strokeWidth = 5;
+///    lineSstyle.stroke = '#7AA20D';
+///    lineSstyle.strokeLineJoin = StrokeLineJoin.round;
+///```
+///
+/// Example2: arc(arcAbs, arcRel)
+///
+/// ![](https://raw.githubusercontent.com/simmone/dart_simple_svg/refs/heads/master/showcase/path/arc.svg)
+///
+///```dart
+///    var arc1Path = Path();
+///    arc1Path.movetoAbs((130, 45));
+///    arc1Path.arcAbs((170.0, 85.0), (80.0, 40.0), ArcDirection.leftBig);
+///    final arc1Id = svg.defShape(arc1Path);
+///
+///    var arc2Path = Path();
+///    arc2Path.movetoAbs((130, 45));
+///    arc2Path.arcAbs((170, 85), (80, 40), ArcDirection.leftSmall);
+///    final arc2Id = svg.defShape(arc2Path);
+///
+///    var arc3Path = Path();
+///    arc3Path.movetoAbs((130, 45));
+///    arc3Path.arcAbs((170, 85), (80, 40), ArcDirection.rightBig);
+///    final arc3Id = svg.defShape(arc3Path);
+///
+///    var arc4Path = Path();
+///    arc4Path.movetoAbs((130, 45));
+///    arc4Path.arcAbs((170, 85), (80, 40), ArcDirection.rightSmall);
+///    final arc4Id = svg.defShape(arc4Path);
+///```
+///
+/// Example3: ccurve(ccurveAbs, ccurveRel)
+///
+/// ![](https://raw.githubusercontent.com/simmone/dart_simple_svg/refs/heads/master/showcase/path/ccurve1.svg)
+///
+///```dart
+///    var ccurvePath = Path();
+///    ccurvePath.movetoAbs((10.0, 60.0));
+///    ccurvePath.ccurveAbs((30.0, 15.0), (80.0, 15.0), (100.0, 60.0));
+///    ccurvePath.ccurveAbs((120, 105), (170, 105), (190, 60));
+///    final ccurveId = svg.defShape(ccurvePath);
+///```
+///
+/// Example4: qcurve(qcurveAbs, qcurveRel)
+///
+/// ![](https://raw.githubusercontent.com/simmone/dart_simple_svg/refs/heads/master/showcase/path/qcurve1.svg)
+///
+///```dart
+///    var qcurvePath = Path();
+///    qcurvePath.movetoAbs((10, 60));
+///    qcurvePath.qcurveAbs((60.0, 10.0), (110.0, 60.0));
+///    qcurvePath.qcurveAbs((160, 110), (210, 60.0));
+///    final qcurveId = svg.defShape(qcurvePath);
+///```
+///
+/// Example5: raw
+///
+/// ![](https://raw.githubusercontent.com/simmone/dart_simple_svg/refs/heads/master/showcase/path/raw_path.svg)
+///
+///```dart
+///    var rawPath = Path();
+///    rawPath.raw(
+///      'M248.761,92c0,9.801-7.93,17.731-17.71,17.731c-0.319,0-0.617,0-0.935-0.021',
+///    );
+///    rawPath.raw(
+///      'c-10.035,37.291-51.174,65.206-100.414,65.206 c-49.261,0-90.443-27.979-100.435-65.334',
+///    );
+///    rawPath.raw(
+///      'c-0.765,0.106-1.531,0.149-2.317,0.149c-9.78,0-17.71-7.93-17.71-17.731',
+///    );
+///    rawPath.raw(
+///      'c0-9.78,7.93-17.71,17.71-17.71c0.787,0,1.552,0.042,2.317,0.149',
+///    );
+///    rawPath.raw(
+///      'C39.238,37.084,80.419,9.083,129.702,9.083c49.24,0,90.379,27.937,100.414,65.228h0.021',
+///    );
+///    rawPath.raw(
+///      'c0.298-0.021,0.617-0.021,0.914-0.021C240.831,74.29,248.761,82.22,248.761,92z',
+///    );
+///    final rawId = svg.defShape(rawPath);
+///```
 class Path extends Shape {
   List<PathAction> actions = [];
 
