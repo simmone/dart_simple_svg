072bf8f3f89c862af602991c22c3612def4f0510
Chen Xiao <chenxiao770117@gmail.com>
2025-08-17 16:02:49 +0800

    update

--- lib/src/define/shape.dart
+++ lib/src/define/shape.dart
@@ -1,11 +1,7 @@
 import 'package:simple_svg/src/define/precision.dart';
 
-class Shape with Precision {
-  String format(String shapeId) {
-    return '';
-  }
+abstract class Shape with Precision {
+  String format(String shapeId);
 
-  String unique() {
-    return '';
-  }
+  String unique();
 }
